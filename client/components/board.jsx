ad1c9360d974cf8bc74611b82d8c70525545e2d6
Timothy Yin <tyin92@gmail.com>
2017-02-23 14:52:27 -0500

    function to check for winner successful

--- client/components/board.jsx
+++ client/components/board.jsx
@@ -6,6 +6,11 @@ export default function Board(props) {
   const style = {
     width: '600px',
     height: '600px',
+    margin: 'auto',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
   }
 
   console.log(board);
--- client/components/game.jsx
+++ client/components/game.jsx
@@ -8,21 +8,53 @@ export default class Game extends Component {
       versus: 'human', // human or AI
       board: Array(9).fill(false), // Default starting board with no values
       xTurn: true,
+      gameOver: false,
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(i) {
-    const newBoard = this.state.board.slice();
-    newBoard[i] = 'X';
-    this.setState({board: newBoard});
-    console.log('CLICKED');
+    //Check if square is already filled
+
+    if (!this.checkWinner(this.state.board) && !this.state.board[i]) {
+      const newBoard = this.state.board.slice();
+      newBoard[i] = this.state.xTurn ? 'X' : 'O';
+        this.setState({
+          board: newBoard,
+          xTurn: !this.state.xTurn,
+        });
+    }
+  }
+
+  checkWinner(board) {
+    let gameOver = false;
+    const checkRows = (rowArr) => {
+      rowArr.forEach((i) => {
+        if (board[i] && board[i] === board[i + 1] && board[i] === board[i + 2]) {
+          gameOver = true;
+        }
+      });
+    };
+    const checkCols = (colArr) => {
+      colArr.forEach((i) => {
+        if (board[i] && board[i] === board[i + 3] && board[i] === board[i + 6]) gameOver = true;
+      });
+    };
+    const checkDiags = () => {
+      if (board[0] && board[0] === board[4] && board[0] === board[8] &&
+          board[2] && board[2] === board[4] && board[2] === board[6]) gameOver = true;
+    };
+    checkRows([0, 3, 6]);
+    checkCols([0, 1, 2]);
+    checkDiags();
+
+    return gameOver;
   }
 
   render() {
     return (
       <div>
-        <h1>GAME</h1>
+        <h1>TIC TAC TOE</h1>
         <Board board={this.state.board} onClick={this.handleClick} />
       </div>
     );
