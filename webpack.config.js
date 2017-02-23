427b963b13856243dff7e29886c6361ac286b5e8
Timothy Yin <tyin92@gmail.com>
2017-02-23 11:49:08 -0500

    Setup webpack and babelrc

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './client/index.jsx'
+  ],
+  module: {
+    loaders: [
+      {test: /\.jsx$/, include: __dirname + '/client', loader: "babel-loader"}
+    ]
+  },
+  output: {
+    filename: "index_bundle.js",
+    path: __dirname + '/dist'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
