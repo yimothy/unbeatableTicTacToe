0023293386ebed676117e2c0d3ac18d1e6886b46
Timothy Yin <tyin92@gmail.com>
2017-02-23 22:23:00 -0500

    Comment out AI tree

--- client/ai/AIDecisionTree.js
+++ client/ai/AIDecisionTree.js
@@ -1,23 +1,26 @@
-class Tree {
-  constructor(data) {
-    const node = new Node(data);
-    this._root = node;
-    this._currNode = node;
-    this._children = [];
-  }
-  addChild(data) {
-    const node = new Tree(data);
-    this._children.push(node);
-  }
-}
-
-//Decision trees when AI goes second
-let AITree2 = {};
-let cornerTree = new Tree("Corner");
-let centerTree = new Tree("Center");
-let edgeTree = new Tree("Edge");
-
-AITree2.addChild()
-
-
-export default decisionTree;
+// class Tree {
+//   constructor(data) {
+//     const node = new Node(data);
+//     this._root = node;
+//     this._currNode = node;
+//     this._children = [];
+//   }
+//   addChild(data) {
+//     const node = new Tree(data);
+//     this._children.push(node);
+//   }
+// }
+//
+// //Decision trees when AI goes second
+// let AITree2 = {};
+// let cornerTree = new Tree({
+//   player: "Human",
+//   move: "corner",
+// });
+// let centerTree = new Tree("Center");
+// let edgeTree = new Tree("Edge");
+//
+// AITree2.addChild()
+//
+//
+// export default decisionTree;
